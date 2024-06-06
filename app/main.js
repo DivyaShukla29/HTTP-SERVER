17cb1347a47cf9243860c2854cd3b6a02d547805
Divya <shukladivya7298@gmail.com>
2024-06-06 14:47:38 +0530

    Response with body

--- app/main.js
+++ app/main.js
@@ -4,42 +4,85 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
+server.getConnections((count, err) => {
+  for (var i = 0; i < count; i++) {
+    const server = net.createServer((socket) => {
    
-  socket.on('data', (data) => {
-    const request = data.toString(); // converting the raw buffer data to string 
-    console.log("Request: \n" + request); 
-    let header;
-    const url = request.split(" ")[1]; // spliting the string to get the url ..split (" ")- returns an arry of these
-    // elements GET, /echo/str -> (url), HTTP / 1.1
-    
-    
-    if (url == "/"){
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    }
-    else if (url.includes("/echo/")) {
-      const str = url.split("/echo/")[1];
-      const l = str.length;
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
-    }
-    else if (url == "/user-agent") {
-      header= request.split("\r\n")[2];  
-      const ua = header.split(": ")[1];
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${ua.length}\r\n\r\n${ua}`);
-      }
-    else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
+      socket.on('data', (data) => {
+        const request = data.toString(); // converting the raw buffer data to string 
+        console.log("Request: \n" + request);
+        let header;
+        const url = request.split(" ")[1]; // spliting the string to get the url ..split (" ")- returns an arry of these
+        // elements GET, /echo/str -> (url), HTTP / 1.1
         
-          
-  });
-
-
-
+        
+        if (url == "/") {
+          socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }
+        else if (url.includes("/echo/")) {
+          const str = url.split("/echo/")[1];
+          const l = str.length;
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
+        }
+        else if (url == "/user-agent") {
+          header = request.split("\r\n")[2];
+          const ua = header.split(": ")[1];
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${ua.length}\r\n\r\n${ua}`);
+        }
+        else {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+               
+      });
+     
+      
+    });
+    if (err) {
+      socket.end();
+      server.close();
+    }
+  }
   socket.on("close", () => {
     socket.end();
     server.close();
   });
-});
+  });
+    // const server = net.createServer((socket) => {
+   
+    //   socket.on('data', (data) => {
+    //     const request = data.toString(); // converting the raw buffer data to string 
+    //     console.log("Request: \n" + request); 
+    //     let header;
+    //     const url = request.split(" ")[1]; // spliting the string to get the url ..split (" ")- returns an arry of these
+    //     // elements GET, /echo/str -> (url), HTTP / 1.1
+    
+    
+    //     if (url == "/"){
+    //       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    //     }
+    //     else if (url.includes("/echo/")) {
+    //       const str = url.split("/echo/")[1];
+    //       const l = str.length;
+    //       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
+    //     }
+    //     else if (url == "/user-agent") {
+    //       header= request.split("\r\n")[2];  
+    //       const ua = header.split(": ")[1];
+    //       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${ua.length}\r\n\r\n${ua}`);
+    //       }
+    //     else {
+    //       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    //     }
+        
+          
+    //   });
+
+
+
+  //   socket.on("close", () => {
+  //     socket.end();
+  //     server.close();
+  //   });
+  // })
 
 server.listen(4221, "localhost");
