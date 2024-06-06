b87b67bb2380c4a4b13d54e105de033b3c541d19
Divya <shukladivya7298@gmail.com>
2024-06-07 00:04:12 +0530

    Post a file

--- app/main.js
+++ app/main.js
@@ -48,12 +48,17 @@ const server = net.createServer((socket) => {
     else {
       if (path.startsWith("/files/")) {
         const directory = process.argv[3];
+        if (!fs.existsSync(directory)) {
+          fs.mkdirSync(directory, { recursive: true });
+        }
         const filename = path.split("/files/")[1];
         const content = req.split("\r\n\r\n")[1];
         fs.writeFileSync('${directory}/${filename}', content);
         socket.write("HTTP/1.1 201 \r\n\r\n");
       }
-
+      else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
 
     }
 
