23c23dd7a9194f95a2e34ae72981f7f50331d1aa
Divya <shukladivya7298@gmail.com>
2024-06-06 12:53:18 +0530

    Response with body

--- app/main.js
+++ app/main.js
@@ -6,19 +6,23 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
    
-    socket.on('data', (data) => {    
-        const request = data.toString();
-        // if (request.startsWith("GET / ")) {
-        //     socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        // }
-        // else {
-        //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      // }
-      const str = "";
-      if (request.startsWith(`GET /echo/${str}`)) {
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${str}`);
-       }
-    });
+  socket.on('data', (data) => {
+    const request = data.toString();
+    // if (request.startsWith("GET / ")) {
+    //     socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    // }
+    // else {
+    //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    // }
+    const arr = request.split(" ");  
+    const req = arr[1].split("/");
+    const str = req[2];
+    const l = str.length;
+    
+   
+    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
+       
+  });
 
 
 
