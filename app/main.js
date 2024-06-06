34bee9ca66216271dc0c097455af753f887f141c
Divya <shukladivya7298@gmail.com>
2024-06-06 13:07:38 +0530

    Response with body

--- app/main.js
+++ app/main.js
@@ -8,28 +8,29 @@ const server = net.createServer((socket) => {
    
   socket.on('data', (data) => {
     const request = data.toString();
+    console.log("Request: \n" + request);
+
     // if (request.startsWith("GET / ")) {
     //     socket.write("HTTP/1.1 200 OK\r\n\r\n");
     // }
     // else {
     //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     // }
-    const arr = request.split(" ");  
-    const req = arr[1].split("/");
-   let str = "";
-        let l = 0;
-
-        // Ensure there are enough parts in the request line
-        if (arr.length > 1) {
-            const req = arr[1].split("/");
-            // Ensure the URL has at least 3 segments (["", "echo", "abc"])
-            if (req.length > 2) {
-                str = req[2];
-                l = str.length;
-            }
-        }
-   
-    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
+    const url = request.split(" ")[1];  
+    if (url == "/"){
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
+    else if (url.includes("/echo/")) {
+      const str = url.split("/echo/")[1];
+      const l = str.length;
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
+    }
+    else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+        
+        
+    
        
   });
 
