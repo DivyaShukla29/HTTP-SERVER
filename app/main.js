8a41a64c99deb496c820c5b06f629d88dfffa487
Divya <shukladivya7298@gmail.com>
2024-06-06 15:49:41 +0530

    Read file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require('node:fs');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -28,14 +29,26 @@ console.log("Logs from your program will appear here!");
               const ua = header.split(": ")[1];
               socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${ua.length}\r\n\r\n${ua}`);
             }
+            else if (url == "/files/") {
+              const file = url.split("/files/")[1];
+              fs.readFile(file, 'utf8', (err, data) => {
+                if (err) {
+                  socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                }
+                else {
+                  socket.write(`HTTP / 1.1 200 OK\r\nContent - Type: application / octet - stream\r\nContent - Length: ${ file.files.length }\r\n\r\n${data}`)
+                }
+              });
+              }
+              
+              
+              
             else {
               socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
             }
           
             
           });
-         
-        
           socket.on("error", () => {
             console.warn(error);
             socket.write("HTTP/1.1 500\r\n\r\n");
@@ -46,40 +59,6 @@ console.log("Logs from your program will appear here!");
             server.close();
           });
         });
-    //   socket.on('data', (data) => {
-    //     const request = data.toString(); // converting the raw buffer data to string 
-    //     console.log("Request: \n" + request); 
-    //     let header;
-    //     const url = request.split(" ")[1]; // spliting the string to get the url ..split (" ")- returns an arry of these
-    //     // elements GET, /echo/str -> (url), HTTP / 1.1
-    
-    
-    //     if (url == "/"){
-    //       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    //     }
-    //     else if (url.includes("/echo/")) {
-    //       const str = url.split("/echo/")[1];
-    //       const l = str.length;
-    //       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${l}\r\n\r\n${str}`);
-    //     }
-    //     else if (url == "/user-agent") {
-    //       header= request.split("\r\n")[2];  
-    //       const ua = header.split(": ")[1];
-    //       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:  ${ua.length}\r\n\r\n${ua}`);
-    //       }
-    //     else {
-    //       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    //     }
-        
-          
-    //   });
-
-
-
-    // socket.on("close", () => {
-    //   socket.end();
-    //   server.close();
-    // });
-  // })
+   
 
 server.listen(4221, "localhost");
